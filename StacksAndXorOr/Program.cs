4c577bc963905e6bc78cabc0739c2df70e1222ec
Zoltan Gaspar <zoltan.gaspar@gmail.com>
2016-11-14 11:56:26 +0100

    initial commit of hackerrank projects

---
+++ StacksAndXorOr/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StacksAndXorOr
+{
+	class Program
+	{
+		static void Main(String[] args)
+		{
+			var n = int.Parse(Console.ReadLine());
+			var rgh = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+			var maxXor = 0L;
+			var stckMin = new Stack<long>();
+			for (var i = 0; i < n; i++)
+			{
+				foreach (var m1 in stckMin)
+				{
+					var x = m1 ^ rgh[i];
+					//Console.WriteLine("{0} xor {1} = {2} ", m1, rgh[i], x);
+					maxXor = Math.Max(x, maxXor);
+					if (m1 < rgh[i])
+						break;
+				}
+
+				while (stckMin.Any() && stckMin.Peek() > rgh[i])
+					stckMin.Pop();
+
+				stckMin.Push(rgh[i]);
+			}
+			Console.WriteLine(maxXor);
+		}
+	}
+}
---
+++ StacksLargestRect/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StacksLargestRect
+{
+	class IH
+	{
+		public int i;
+		public int h;
+	}
+
+	class Solution {
+		static void Main(String[] args) {
+			var n = int.Parse(Console.ReadLine());
+			var rgh = Console.ReadLine().Split(' ').Select(st => int.Parse(st)).ToArray();
+			var maxRect = 0;
+			var stckIH = new Stack<IH>();
+			var TT = new IH{i = int.MaxValue, h = 0 };
+			for (var i = 0; i < n; i++)
+			{
+				var prevIH = TT;
+				while (true)
+				{
+					var ih = stckIH.Any() ? stckIH.Peek() : TT;
+					if (rgh[i] >= ih.h)
+						break;
+					prevIH = stckIH.Any() ? stckIH.Pop() : TT;
+					maxRect = Math.Max(prevIH.h * (i - prevIH.i), maxRect);
+				}
+				var ihPeek = stckIH.Any() ? stckIH.Peek() : TT;
+				if (ihPeek.h < rgh[i])
+				{
+					stckIH.Push(new IH {i = Math.Min(i, prevIH.i), h = rgh[i]});
+				}
+
+			}
+			maxRect = stckIH.Select(ih => ih.h * (n - ih.i)).Concat(new[] {maxRect}).Max();
+			Console.WriteLine(maxRect);
+		}
+	}
+}
